76eb02df174137f397d43620461e19c73e35eefe
Noela-Murugi <murugin254@gmail.com>
2022-08-15 14:10:58 +0300

    completed the assignment

--- index.js
+++ index.js
@@ -1,1 +1,5 @@
 // Write your code in this file!
+const currentUser = 'Noela Murugi';
+const welcomeMessage = `Welcome to Flatbook, ${currentUser}!`;
+const excitedWelcomeMessage = `${welcomeMessage.toUpperCase()}, ${currentUser}!`;
+const shortGreeting = `Welcome, ${currentUser.slice(0, 1)}!`;
